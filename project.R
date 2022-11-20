eec3988f24ab479dd3ba48c67f4cbd3060844c5b
Dwika <dwikariswanda@gmail.com>
2022-11-20 17:18:42 +0700

    Fix error

--- project.R
+++ project.R
@@ -1,6 +1,9 @@
 library(readxl)
+library(tidyverse)
 library(here)
+library(dplyr)
 
-file <- read_excel(path = here("Project DS/data-raw", "Smartphone_usage_and_Intenational_Tourist_Behavior.xlsx"))
+file <- read_excel(path = here("data-raw/Smartphone_usage_and_Intenational_Tourist_Behavior.xlsx"))
 head(file)
+
 
